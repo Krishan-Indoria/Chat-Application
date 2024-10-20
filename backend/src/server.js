856b683dd74c47bdeb07e0813c08b112dfefac85
Krishan-Indoria <krishansaini2410@gmail.com>
2024-10-20 21:11:13 +0530

    connect to database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
-import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+import dbConnection from './config/dbConfig.js';
+const uri = process.env.REMOTE_DB_URL + process.env.REMOTE_DB; 
+dbConnection(uri);
 
 app.get('/', (req,res) =>{
     res.send("<h1>Hello World!</h1>");
